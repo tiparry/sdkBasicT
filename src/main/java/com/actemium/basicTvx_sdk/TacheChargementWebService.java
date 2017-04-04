94995be58c8c9d6f7dad42280e8cd4fd13e654a4
CONRAD Anthony <Anthony.CONRAD@vinci-energies.net>
2017-04-04 17:45:09 +0200

    setEstCharge les objets fils

--- src/main/java/com/actemium/basicTvx_sdk/TacheChargementWebService.java
+++ src/main/java/com/actemium/basicTvx_sdk/TacheChargementWebService.java
@@ -1,6 +1,10 @@
 package com.actemium.basicTvx_sdk;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.Callable;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -10,10 +14,13 @@ import org.xml.sax.SAXException;
 import com.actemium.basicTvx_sdk.restclient.RestException;
 
 import giraudsa.marshall.exception.InstanciationException;
+import utils.TypeExtension;
+import utils.champ.Champ;
 
 class TacheChargementWebService implements Callable<Object> {
 	private final GlobalObjectManager gom;
 	private final Object objetATraiter;
+
 	public TacheChargementWebService(Object objetATraiter, GlobalObjectManager gom) {
 		super();
 		this.gom = gom;
@@ -21,14 +28,52 @@ class TacheChargementWebService implements Callable<Object> {
 	}
 
 	@Override
-	public Object call() throws IOException, RestException, SAXException, InstanciationException, InterruptedException, ParserConfigurationException, ReflectiveOperationException {
+	public Object call() throws IOException, RestException, SAXException, InstanciationException, InterruptedException, ParserConfigurationException,
+			ReflectiveOperationException {
 		if (!Thread.currentThread().isInterrupted()) {
 			String id = gom.getId(objetATraiter);
 			Class<?> clazz = objetATraiter.getClass();
 			Object o = gom.persistanceManager.getObjectById(clazz, id, gom);
-			if(o != null)
+			if (o != null) {
 				gom.setEstCharge(objetATraiter);
+				setFilsEstCharge(objetATraiter);
+			}
 		}
 		return objetATraiter;
-	}   
+	}
+
+	private void setFilsEstCharge(Object objetPere) throws IllegalAccessException {
+		if (objetPere == null)
+			return;
+		List<Champ> champs = TypeExtension.getSerializableFields(objetPere.getClass());
+		for (Champ champ : champs) {
+			Object value = champ.get(objetPere);
+			if (!champ.isSimple() && value != null) {
+				if (value instanceof Collection<?>)
+					traiteCollection((Collection<?>) value);
+				else if (value instanceof Map<?, ?>)
+					traiteMap((Map<?, ?>) value);
+				else // objet
+					gom.setEstCharge(value);
+			}
+
+		}
+	}
+
+	private void traiteCollection(Collection<?> value) {
+		for (Object o : value)
+			if(o != null && !TypeExtension.isSimple(o.getClass()))
+				gom.setEstCharge(o);
+	}
+	
+	private void traiteMap(Map<?, ?> map) {
+		for(Entry<?,?> entry : map.entrySet()) {
+			Object k = entry.getKey();
+			Object v = entry.getValue();
+			if(k != null && !TypeExtension.isSimple(k.getClass()))
+				gom.setEstCharge(k);
+			if(v != null && !TypeExtension.isSimple(v.getClass()))						
+				gom.setEstCharge(v);
+		}
+	}
 }
